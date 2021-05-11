1332ac497d4c3b5ef4c139eab014a3f6dab04e0f
nddmanh <manhnd.ilec@gmail.com>
2021-05-11 22:35:39 +0700

    submit lab section-section6

---
+++ section-6/js/main.js
@@ -0,0 +1,20 @@
+admin = [
+    {
+        username: "admin",
+        password: "123456"
+    }
+]
+star = [
+    {
+        id: 1,
+        name: "Lê thu trang",
+        describe: "Gọi em là em bé",
+        price: "15k/h",
+        link: ""
+    }
+]
+
+ 
+star.forEach(element => {
+    
+});
---
+++ section-6/js/star.js
@@ -0,0 +1,18 @@
+admin = [
+    {
+        username: "admin",
+        password: "123456"
+    }
+]
+star = [
+    {
+        id: 1,
+        name: "Lê thu trang",
+        describe: "Gọi em là em bé",
+        price: "15k/h"
+    }
+]
+
+array.forEach(element => {
+    
+});
