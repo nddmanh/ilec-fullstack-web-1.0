d47be56557830858b04cf6d014f29dc734f1b74a
nddmanh <manhnd.ilec@gmail.com>
2021-06-07 18:53:18 +0700

    submit section-12

---
+++ section-12/config/index.js
@@ -0,0 +1,5 @@
+  
+module.exports = {
+    MONGO_CONNECTION_URL: 'mongodb://localhost:27017/ilec-web',
+    PORT: 3000
+};
---
+++ section-12/modules/students/student.service.js
@@ -0,0 +1,56 @@
+const studentRepository = require('./student.repository');
+const repository = require('./student.repository');
+
+
+const find = function (query) {
+
+  const limit = Number(query.limit);
+  const offset = Number(query.offset);
+
+  delete query.limit; 
+  delete query.offset;
+
+  
+  // Transform query - Data validation
+  const supportedQueryFields = ['fullName', 'phoneNum', 'className'];
+
+  Object.keys(query).forEach(function (key) {
+    if (!supportedQueryFields.includes(key)) {
+      throw new Error(`Unrecognized field: ${key}`);
+    }
+  });
+
+  if (query.fullName) {
+    query.fullName = { $regex: query.fullName, $options: 'i' };
+  }
+
+  // Querying
+  return repository.find(query, limit, offset);
+}
+
+const create = function (inputs) {
+  // Business logic
+
+  // Data validation
+
+  // Persist data
+  return repository.create(inputs);
+}
+
+const update = function (id, newObject, cb) {
+  // Business logic
+
+  // Persist data
+  repository.update(id, newObject, cb);
+}
+
+const remove = function (id, cb) {
+  repository.remove(id, cb);
+}
+
+module.exports = {
+  find: find,
+  create: create,
+  update: update,
+  remove: remove
+};
